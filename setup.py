e0e09538f8eeab603a5fe9611580dda5eaf11e95
Ben Dichter <ben.dichter@gmail.com>
2020-01-16 22:56:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup_args = {
     'name': 'ndx-grayscalevolume',
-    'version': '0.0.2',
+    'version': '0.1.0',
     'description': '3D grayscale image.',
     'long_description': long_description,
     'long_description_content_type="text/markdown",
